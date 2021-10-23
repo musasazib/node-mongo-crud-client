55d57a43c3c8aaab6ebc000a7736598d9cc56c99
musasazib <musasazib@gmail.com>
2021-10-23 16:24:06 +0600

    UI show on single user

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const UpdateUser = () => {
+    const { id } = useParams();
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        const url = `http://localhost:5000/users/${id}`;
+        fetch(url)
+            .then(rsc => rsc.json())
+            .then(data => setUser(data))
+    }, [id])
     return (
         <div>
-            <h2>This is Update user</h2>
+            <h3>Update: {user.name}</h3>
+            <p><small>{id}</small></p>
         </div>
     );
 };
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
@@ -35,8 +36,8 @@ const Users = () => {
                     users.map(user => <li
                         key={user._id}
                     >{user.name} = {user.email}
+                        <Link to={`/users/update/${user._id}`}><button>Update</button></Link>
                         <button onClick={() => handleDeleteUser(user._id)}>X</button>
-                        <button>Update</button>
                     </li>)
                 }
             </ul>
